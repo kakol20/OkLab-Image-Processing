0335422aeed5e9464d5ace5ca5bce7a9a1a504e8
Adrian Aquino-Neary <adrianaquino.aquino33@gmail.com>
2024-05-24 23:42:35 +0100

    Initial Upload
    Taken from https://github.com/kakol20/Image-Hue-Change with some deletions

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,34 @@
-﻿// main.cpp : Defines the entry point for the application.
-//
+﻿#include <chrono>
+#include <fstream>
+#include <iostream>
+#include <sstream>
 
-#include "main.h"
+#include "../ext/json/json.hpp"
+#include "colourSpace/ColourSpace.hpp"
+#include "colourSpace/OkLCh.h"
+#include "colourSpace/sRGB.hpp"
+#include "image/Image.h"
+#include "maths/Maths.hpp"
+#include "misc/Log.h"
 
-int main(int argc, char* argv[]) {
-  std::cout << "Hello World\n";
+using json = nlohmann::json;
+
+const double Maths::Pi = 3.1415926535;
+const double Maths::Tau = 6.283185307;
+const double Maths::RadToDeg = 180. / Maths::Pi;
+const double Maths::DegToRad = Maths::Pi / 180.;
+
+int main() {
+  // ----- ENTER CODE HERE -----
+
+  Log::StartLine();
+  Log::Write("Hello World!");
+  Log::EndLine();
 
   std::cout << "Press enter to exit...\n";
   std::cin.ignore();
+
+  Log::Save();
+
   return 0;
 }
